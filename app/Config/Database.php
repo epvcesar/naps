45eb5a9e44a4c004d532ee6444ff48dcf52800be
emanuelpv <emanuel@rumoaesfcex.com.br>
2025-10-22 09:12:28 -0300

    atualizacao

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => 'XSX2qiGKM3',
+        'password'     => 'naps',
         'database'     => 'naps',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
